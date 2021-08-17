d4c19fa7ec9a5f623a1528557be4a94c7a0233dd
theo_prat <theo_prat@ens.univ-artois.fr>
2021-08-17 20:30:37 +0200

    ajout du background et choix de la photo pour Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="home">
-   <h1>TEAM BEAST</h1>
-    <p>Aussi puissant qu'une mouche</p>
+    <div class="background">
+      <div class="blur"></div>
+    </div>
+      <h1>TEAM BEAST</h1>
+      <p>Aussi puissant qu'une mouche</p>
   </div>
 </template>
+<style>
+body{
+  background-image: url("../assets/backgroundKeyboard.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-position-y: -150px;
+  /*backdrop-filter: blur(8px);*/
+}
 
+</style>
 <script>
 export default {
   name: 'Home'
