bd9b5ef47cb255dbfa8c5d02396450e8f69ef6f7
PanDDaB <PanDDaB@users.noreply.github.com>
2021-08-22 13:39:47 +0200

    Page Home Fini

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,14 @@
     <nav class="topnav">
       <a href="#" v-on:click="openNav()">
         <svg width="30" height="30" id="icoOpen">
-          <path d="M0,5 30,5" stroke="#000" stroke-width="5"/>
-          <path d="M0,14 30,14" stroke="#000" stroke-width="5"/>
-          <path d="M0,23 30,23" stroke="#000" stroke-width="5"/>
+          <path d="M0,5 30,5" stroke="#ffff" stroke-width="5"/>
+          <path d="M0,14 30,14" stroke="#ffff" stroke-width="5"/>
+          <path d="M0,23 30,23" stroke="#ffff" stroke-width="5"/>
         </svg>
       </a>
     </nav>
+
+    <img src="../src/assets/logoBeast.svg" alt="mais c'est le logo" class="logo">
 
 
 
@@ -26,12 +28,28 @@
 </template>
 
 <style>
+
+body{
+  background-color: black;
+  /*ou background-color: rgb(15,16,19);*/
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+}
+
+.logo{
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  top: 20px; right: 20px;
+  z-index: 100;
+  border: 2px solid black;
+  background-color: white;
+  border-radius: 25px;
+
 }
 
 #nav {
@@ -55,7 +73,7 @@
   height: 100%; /* 100% Full-height */
   width: 0; /* 0 width - change this with JavaScript */
   position: fixed; /* Stay in place */
-  z-index: 1; /* Stay on top */
+  z-index: 2; /* Stay on top */
   top: 0;
   left: 0;
   background-color: #111; /* Black*/
@@ -84,7 +102,7 @@
   position: absolute;
   top: 0;
   right: 25px;
-  font-size: 36px;
+  font-size: 45px;
   margin-left: 50px;
 }
 
@@ -103,6 +121,7 @@ body {
 .topnav {
   overflow: hidden;
   position: absolute;
+  z-index: 1;
 }
 
 /* Style the links inside the navigation bar */
@@ -179,9 +198,365 @@ a{
 }
 }
 
+
+/*normalized*/
+/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
+
+/* Document
+   ========================================================================== */
+
+/**
+ * 1. Correct the line height in all browsers.
+ * 2. Prevent adjustments of font size after orientation changes in iOS.
+ */
+
+html {
+  line-height: 1.15; /* 1 */
+  -webkit-text-size-adjust: 100%; /* 2 */
+  scroll-behavior: smooth;
+}
+
+/* Sections
+   ========================================================================== */
+
+/**
+ * Remove the margin in all browsers.
+ */
+
+body {
+  margin-left: 0;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+/**
+ * Render the `main` element consistently in IE.
+ */
+
+main {
+  display: block;
+}
+
+/**
+ * Correct the font size and margin on `h1` elements within `section` and
+ * `article` contexts in Chrome, Firefox, and Safari.
+ */
+
+h1 {
+  font-size: 2em;
+  margin: 0.67em 0;
+}
+
+/* Grouping content
+   ========================================================================== */
+
+/**
+ * 1. Add the correct box sizing in Firefox.
+ * 2. Show the overflow in Edge and IE.
+ */
+
+hr {
+  box-sizing: content-box; /* 1 */
+  height: 0; /* 1 */
+  overflow: visible; /* 2 */
+}
+
+/**
+ * 1. Correct the inheritance and scaling of font size in all browsers.
+ * 2. Correct the odd `em` font sizing in all browsers.
+ */
+
+pre {
+  font-family: monospace, monospace; /* 1 */
+  font-size: 1em; /* 2 */
+}
+
+/* Text-level semantics
+   ========================================================================== */
+
+/**
+ * Remove the gray background on active links in IE 10.
+ */
+
+a {
+  background-color: transparent;
+}
+
+/**
+ * 1. Remove the bottom border in Chrome 57-
+ * 2. Add the correct text decoration in Chrome, Edge, IE, Opera, and Safari.
+ */
+
+abbr[title] {
+  border-bottom: none; /* 1 */
+  text-decoration: underline; /* 2 */
+  text-decoration: underline dotted; /* 2 */
+}
+
+/**
+ * Add the correct font weight in Chrome, Edge, and Safari.
+ */
+
+b,
+strong {
+  font-weight: bolder;
+}
+
+/**
+ * 1. Correct the inheritance and scaling of font size in all browsers.
+ * 2. Correct the odd `em` font sizing in all browsers.
+ */
+
+code,
+kbd,
+samp {
+  font-family: monospace, monospace; /* 1 */
+  font-size: 1em; /* 2 */
+}
+
+/**
+ * Add the correct font size in all browsers.
+ */
+
+small {
+  font-size: 80%;
+}
+
+/**
+ * Prevent `sub` and `sup` elements from affecting the line height in
+ * all browsers.
+ */
+
+sub,
+sup {
+  font-size: 75%;
+  line-height: 0;
+  position: relative;
+  vertical-align: baseline;
+}
+
+sub {
+  bottom: -0.25em;
+}
+
+sup {
+  top: -0.5em;
+}
+
+/* Embedded content
+   ========================================================================== */
+
+/**
+ * Remove the border on images inside links in IE 10.
+ */
+
+img {
+  border-style: none;
+}
+
+/* Forms
+   ========================================================================== */
+
+/**
+ * 1. Change the font styles in all browsers.
+ * 2. Remove the margin in Firefox and Safari.
+ */
+
+button,
+input,
+optgroup,
+select,
+textarea {
+  font-family: inherit; /* 1 */
+  font-size: 100%; /* 1 */
+  line-height: 1.15; /* 1 */
+  margin: 0; /* 2 */
+}
+
+/**
+ * Show the overflow in IE.
+ * 1. Show the overflow in Edge.
+ */
+
+button,
+input { /* 1 */
+  overflow: visible;
+}
+
+/**
+ * Remove the inheritance of text transform in Edge, Firefox, and IE.
+ * 1. Remove the inheritance of text transform in Firefox.
+ */
+
+button,
+select { /* 1 */
+  text-transform: none;
+}
+
+/**
+ * Correct the inability to style clickable types in iOS and Safari.
+ */
+
+button,
+[type="button"],
+[type="reset"],
+[type="submit"] {
+  -webkit-appearance: button;
+}
+
+/**
+ * Remove the inner border and padding in Firefox.
+ */
+
+button::-moz-focus-inner,
+[type="button"]::-moz-focus-inner,
+[type="reset"]::-moz-focus-inner,
+[type="submit"]::-moz-focus-inner {
+  border-style: none;
+  padding: 0;
+}
+
+/**
+ * Restore the focus styles unset by the previous rule.
+ */
+
+button:-moz-focusring,
+[type="button"]:-moz-focusring,
+[type="reset"]:-moz-focusring,
+[type="submit"]:-moz-focusring {
+  outline: 1px dotted ButtonText;
+}
+
+/**
+ * Correct the padding in Firefox.
+ */
+
+fieldset {
+  padding: 0.35em 0.75em 0.625em;
+}
+
+/**
+ * 1. Correct the text wrapping in Edge and IE.
+ * 2. Correct the color inheritance from `fieldset` elements in IE.
+ * 3. Remove the padding so developers are not caught out when they zero out
+ *    `fieldset` elements in all browsers.
+ */
+
+legend {
+  box-sizing: border-box; /* 1 */
+  color: inherit; /* 2 */
+  display: table; /* 1 */
+  max-width: 100%; /* 1 */
+  padding: 0; /* 3 */
+  white-space: normal; /* 1 */
+}
+
+/**
+ * Add the correct vertical alignment in Chrome, Firefox, and Opera.
+ */
+
+progress {
+  vertical-align: baseline;
+}
+
+/**
+ * Remove the default vertical scrollbar in IE 10+.
+ */
+
+textarea {
+  overflow: auto;
+}
+
+/**
+ * 1. Add the correct box sizing in IE 10.
+ * 2. Remove the padding in IE 10.
+ */
+
+[type="checkbox"],
+[type="radio"] {
+  box-sizing: border-box; /* 1 */
+  padding: 0; /* 2 */
+}
+
+/**
+ * Correct the cursor style of increment and decrement buttons in Chrome.
+ */
+
+[type="number"]::-webkit-inner-spin-button,
+[type="number"]::-webkit-outer-spin-button {
+  height: auto;
+}
+
+/**
+ * 1. Correct the odd appearance in Chrome and Safari.
+ * 2. Correct the outline style in Safari.
+ */
+
+[type="search"] {
+  -webkit-appearance: textfield; /* 1 */
+  outline-offset: -2px; /* 2 */
+}
+
+/**
+ * Remove the inner padding in Chrome and Safari on macOS.
+ */
+
+[type="search"]::-webkit-search-decoration {
+  -webkit-appearance: none;
+}
+
+/**
+ * 1. Correct the inability to style clickable types in iOS and Safari.
+ * 2. Change font properties to `inherit` in Safari.
+ */
+
+::-webkit-file-upload-button {
+  -webkit-appearance: button; /* 1 */
+  font: inherit; /* 2 */
+}
+
+/* Interactive
+   ========================================================================== */
+
+/*
+ * Add the correct display in Edge, IE 10+, and Firefox.
+ */
+
+details {
+  display: block;
+}
+
+/*
+ * Add the correct display in all browsers.
+ */
+
+summary {
+  display: list-item;
+}
+
+/* Misc
+   ========================================================================== */
+
+/**
+ * Add the correct display in IE 10+.
+ */
+
+template {
+  display: none;
+}
+
+/**
+ * Add the correct display in IE 10.
+ */
+
+[hidden] {
+  display: none;
+}
 </style>
 
 <script>
+
+
 export default {
   methods: {
     openNav:  function () {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,172 @@
 <template>
   <div class="home">
-    <div class="background">
-      <div class="blur"></div>
-    </div>
+      <div class="background1"></div>
+
+    <div class="texte">
       <h1>TEAM BEAST</h1>
-      <p>Aussi puissant qu'une mouche</p>
-  </div>
+
+      <button id="boutton" v-on:click="openContent()"><a href="#content">DECOUVRIR</a></button>
+    </div>
+
+
+
+    <div id="content" style="display: none">
+      <h2>D'où vient-on</h2>
+      <p class="textLeft">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum, odio vel cursus consectetur, libero urna bibendum tellus, quis dignissim massa mi a orci. Vestibulum in felis convallis, pulvinar lectus non, porta sem. Morbi lacinia quis massa ac viverra. Etiam vehicula elit eget fermentum vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus semper fermentum molestie. Nam euismod posuere tellus, et scelerisque ex. Nulla dignissim interdum commodo. Nulla fermentum mi sed nisi faucibus, nec dignissim dui finibus. Vestibulum scelerisque, eros vel malesuada finibus, turpis neque tempor purus, nec aliquet tellus purus ut erat. Curabitur eleifend id ex ut aliquet. Nam vestibulum convallis ullamcorper. Suspendisse porta commodo faucibus. Nam ullamcorper, felis sed venenatis aliquam, velit nulla dictum nibh, ac consectetur est augue lobortis velit.</p>
+
+      <img src="../assets/photoPote.jpg" alt="photo des coupains" id="imageTeam">
+
+      <h2 class="titleRight">Mais qui somme nous ?</h2>
+      <p class="textRight">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum, odio vel cursus consectetur, libero urna bibendum tellus, quis dignissim massa mi a orci. Vestibulum in felis convallis, pulvinar lectus non, porta sem. Morbi lacinia quis massa ac viverra. Etiam vehicula elit eget fermentum vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus semper fermentum molestie. Nam euismod posuere tellus, et scelerisque ex. Nulla dignissim interdum commodo. Nulla fermentum mi sed nisi faucibus, nec dignissim dui finibus. Vestibulum scelerisque, eros vel malesuada finibus, turpis neque tempor purus, nec aliquet tellus purus ut erat. Curabitur eleifend id ex ut aliquet. Nam vestibulum convallis ullamcorper. Suspendisse porta commodo faucibus. Nam ullamcorper, felis sed venenatis aliquam, velit nulla dictum nibh, ac consectetur est augue lobortis velit.</p>
+
+      <img src="../assets/coque-tel.jpg" alt="coque de téléphone" id="imageCoque">
+    </div>
+
+
+    </div>
 </template>
+
+
+
 <style>
-body{
-  background-image: url("../assets/backgroundKeyboard.jpg");
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: center;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  background-position-y: -150px;
-  /*backdrop-filter: blur(8px);*/
+/*Objets de base*/
+h1{
+  color: white;
+  font-family: 'Archivo Black', arial;
+  font-size: 90px;
+  letter-spacing: 8px;
 }
 
+a {
+  text-decoration: none;
+  color: inherit;
+
+}
+
+p{
+  letter-spacing: 1px;
+}
+
+
+
+/*Les classes*/
+
+/*background */
+.background1{
+  /* The image used */
+  background-image: url("../assets/backgroundKeyboard.jpg");
+
+  /* Add the blur effect */
+  filter: blur(8px);
+  -webkit-filter: blur(8px);
+
+  /* Full height */
+  height: 920px;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+
+}
+
+.texte{
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0, 0.5); /* Black w/opacity/see-through */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  width: 40%;
+  padding: 20px;
+  text-align: center;
+}
+
+.textLeft{
+  margin-right: 60%;
+  margin-top: 50px;
+  margin-left: 10%;
+  text-justify: inter-word;
+}
+
+.textRight{
+  margin-left: 60%;
+  margin-top: 100px;
+  margin-right: 10%;
+  text-justify: inter-word;
+}
+
+.titleRight{
+  margin-top: 150px;
+}
+
+/*Les Ids*/
+
+#boutton{
+  color: white;
+  font-family: Calibri;
+  font-size: 25px;
+  border: 2px white ;
+  font-weight: normal;
+  letter-spacing: 5px;
+  background-color: transparent;
+  border: solid white ;
+  vertical-align: center;
+  text-align: center;
+  display: inline-block;
+  padding: 10px;
+  width: 220px;
+  transition-duration: 0.2s;
+}
+
+
+
+#boutton:hover{
+  background-color: white;
+  cursor: pointer;
+  width: 200px;
+  transition-duration: 250ms;
+  color: black;
+}
+
+#imageTeam{
+  width: 700px;
+  position: absolute;
+  top: 1000px; right: 10%;
+}
+
+#imageCoque{
+  width: 700px;
+  position: absolute;
+  top: 1600px; left: 10%;
+}
+
+#content{
+  margin-top: 100px;
+  color: white;
+}
+
+
 </style>
+
+
+
+
+
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  methods:{
+    openContent: function (){
+      var content = document.getElementById('content');
+      content.style.display = 'inline-block';
+
+      // var content = document.getElementById('example');
+      // content.style.color = 'blue';
+
+    }
+  }
 }
 </script>
